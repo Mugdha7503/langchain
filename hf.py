8cbcf2438c7367ea04377692ff6b00ddd0dc6b09
Mugdha7503 <mugdhaupadhyay0705@gmail.com>
2025-11-18 13:15:12 +0530

    Initial commit

---
+++ hf.py
@@ -0,0 +1,31 @@
+# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = HuggingFaceEndpoint(
+#     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     task="text-generation"
+# )
+
+# model = ChatHuggingFace(llm=llm)
+
+# result = model.invoke("What is the capital of India")
+
+# print(result.content)
+
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
