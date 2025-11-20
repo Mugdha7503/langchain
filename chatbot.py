9ae61b13d44e3b4c90d60dd39c31ca1f4e2cdc78
Mugdha7503 <mugdhaupadhyay0705@gmail.com>
2025-11-20 22:30:29 +0530

    changes

--- chatbot.py
+++ chatbot.py
@@ -16,14 +16,12 @@ ytt_api = YouTubeTranscriptApi()
 try:
     transcript_list = ytt_api.fetch(video_id,languages=["en"])
     raw = transcript_list.to_raw_data()
-   
     transcript = " ".join(chunk["text"] for chunk in raw)
    
-    
 
 except TranscriptsDisabled:
     print("No captions available for this video.")
-
+print(transcript)
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.create_documents([transcript])
@@ -44,8 +42,11 @@ def ask_chatbot(query: str):
     context = "\n\n".join([doc.page_content for doc in docs])
 
     prompt = f"""
-    Use ONLY the following transcript to answer the question.
-    If answer is unknown, say "I don't know".
+    You are a helpful AI chatbot answering questions ONLY using the following transcript content.
+
+    - If information is not available, reply: "I don't know based on the video."
+    - Do not hallucinate or add outside infowhat is the meaning og mind-boogling from the textrmation.
+    - Keep answers clear and concise.
 
     Transcript:
     {context}
@@ -54,9 +55,17 @@ def ask_chatbot(query: str):
     """
 
     response = model.invoke(prompt)
-    return response.content
+    return response.content.strip()
 
+print("\n Chatbot is ready! Ask anything about the video. Type 'exit' to quit.\n")
+while True:
+    user_input = input("You: ").strip()
 
-print(ask_chatbot("What is the video mainly about?"))
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("Bot: Goodbye!")
+        break
+
+    answer = ask_chatbot(user_input)
+    print("Bot:", answer)
 
 
